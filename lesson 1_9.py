6a991f3967c12b0ccd730ae499c791d46ad33eb1
NikiAlexs <nikilexs@gmail.com>
2024-11-21 22:28:18 +0700

    Add files via upload

---
+++ lesson 1_9.py
@@ -0,0 +1,20 @@
+from tkinter.scrolledtext import example
+
+name = 'NikiAlexs'
+print("Hello "+name)
+print("Hello "*3)
+print(name[0])  #нумерация начинается с 0
+print(name[0:4])
+print(name[4:9])
+print(name[0:4:2])
+print(name[:4])
+print(name[4:])
+print(name[::-1])
+
+# Практическое задание
+example = "Homework"
+print(example[0])
+print(example[-1])
+print(example[5:8])
+print(example[::-1])
+print(example[1:9:2])
